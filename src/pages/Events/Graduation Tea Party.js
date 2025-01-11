f343201b9fea77b1739b27c015e476eac41398fa
raunitpatel <raunit@iitg.ac.in>
2025-01-11 22:48:55 +0530

    added banner images

--- src/pages/Events/Graduation Tea Party.js
+++ src/pages/Events/Graduation Tea Party.js
@@ -37,7 +37,11 @@ const Event = () => {
 
   return (
     <div className="event-container">
-      <div className="header">
+      <div className="header"
+      style={{
+        background: `linear-gradient(0deg, rgba(19, 19, 19, 0.86) 0%, rgba(19, 19, 19, 0.00) 100%),
+        url('/image/Events/gtp_banner.jpg') lightgray 0px -75px / 100% 114.94% no-repeat`}}
+      >
         <div className="header-content">
           <h1>Graduation Tea Party</h1>
           <p>
--- src/pages/Events/Smriti.js
+++ src/pages/Events/Smriti.js
@@ -37,7 +37,11 @@ const Event = () => {
 
   return (
     <div className="event-container">
-      <div className="header">
+      <div className="header" 
+    //    style={{
+    // background: `linear-gradient(0deg, rgba(19, 19, 19, 0.86) 0%, rgba(19, 19, 19, 0.00) 100%),
+    // url('/image/Events/smriti_banner.jpg') lightgray 0px -75px / 100% 114.94% no-repeat`}}
+    >
         <div className="header-content">
           <h1>Smriti</h1>
           <p>
